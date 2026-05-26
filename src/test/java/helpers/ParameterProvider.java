14deb6a0a272d25b6a475602de534e7630b86155
Nikita Haritonov <nikita.harit@mail.ru>
2026-05-26 18:11:05 +0300

    feat(helpers): add ParameterProvider singleton for config management

---
+++ src/test/java/helpers/ParameterProvider.java
@@ -0,0 +1,39 @@
+package helpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ParameterProvider {
+    private static final String PARAMETERS_PATH = "configurations/config.properties";
+
+    private static ParameterProvider instance;
+    private final Map<String, String> parameters;
+
+    private ParameterProvider() {
+        try {
+            parameters = new HashMap<>();
+            Properties prop = new Properties();
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PARAMETERS_PATH);
+            if (inputStream == null) {
+                throw new RuntimeException("File not found: " + PARAMETERS_PATH);
+            }
+            prop.load(inputStream);
+            prop.stringPropertyNames()
+                    .forEach(key -> parameters.put(key, prop.getProperty(key)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String get(String key) {
+        if (instance == null) {
+            synchronized (ParameterProvider.class) {
+                instance = new ParameterProvider();
+            }
+        }
+        return instance.parameters.get(key);
+    }
+}
