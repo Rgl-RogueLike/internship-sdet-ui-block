50e16502c585a29bb56a11707a1b705d45c1db9b
Nikita Haritonov <nikita.harit@mail.ru>
2026-05-29 10:12:54 +0300

    test(homepage): add verify contacts (phones, Skype, Email, socials)

--- src/test/java/com/haritonov/uitests/tests/homepage/HomePageTest.java
+++ src/test/java/com/haritonov/uitests/tests/homepage/HomePageTest.java
@@ -17,4 +17,17 @@ public class HomePageTest extends BaseTest {
         Assert.assertTrue(homePage.isCourseSectionVisible(), "Courses section should be visible");
         Assert.assertTrue(homePage.isFooterVisible(), "Footer should be visible");
     }
+
+    @Test
+    public void headerContactsShouldBePresentWhenHomePageLoads() {
+        HomePage homePage = new HomePage(driver, waiter);
+        Assert.assertTrue(homePage.isPageLoaded(), "Home page should be loaded");
+        Assert.assertTrue(homePage.getHeaderPhoneCount() > 0, "Header should contain phone numbers");
+        Assert.assertTrue(homePage.isSkypeLinkPresent(), "Header should contain Skype link");
+        Assert.assertTrue(homePage.isEmailLinkPresent(), "Header should contain Email link");
+        Assert.assertTrue(homePage.isFacebookLinkPresent(), "Header should contain Facebook link");
+        Assert.assertTrue(homePage.isLinkedinLinkPresent(), "Header should contain LinkedId link");
+        Assert.assertTrue(homePage.isInstagramLinkPresent(), "Header should contain Instagram link");
+        Assert.assertTrue(homePage.isYoutubeLinkPresent(), "Header should contain YouTube link");
+    }
 }
