89494e3360d3eb8be1d9f5ee1e898c2b66fa1ac8
Nikita Haritonov <nikita.harit@mail.ru>
2026-05-30 16:21:48 +0300

    tests(login): initialize page in @BeforeMethod, add logout test

--- src/test/java/com/haritonov/uitests/tests/login/LoginPageTest.java
+++ src/test/java/com/haritonov/uitests/tests/login/LoginPageTest.java
@@ -4,14 +4,23 @@ import com.haritonov.uitests.helpers.ParameterProvider;
 import com.haritonov.uitests.pages.LoginPage;
 import com.haritonov.uitests.tests.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginPageTest extends BaseTest {
 
+    private LoginPage loginPage;
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        driver.get(ParameterProvider.get("login.url"));
+        loginPage = new LoginPage(driver);
+    }
+
     @Test
     public void fieldsShouldBeVisibleAndLoginButtonDisableWhenEmpty() {
-        driver.get(ParameterProvider.get("login.url"));
-        LoginPage loginPage = new LoginPage(driver);
         Assert.assertTrue(loginPage.isPageLoaded(), "Login page should be loaded");
         Assert.assertTrue(loginPage.isUsernameFieldVisible(), "Username field should be visible");
         Assert.assertTrue(loginPage.isPasswordFieldVisible(), "Password field should be visible");
@@ -20,9 +29,6 @@ public class LoginPageTest extends BaseTest {
 
     @Test
     public void shouldLoginSuccessfullyWithValidCredentials() {
-        driver.get(ParameterProvider.get("login.url"));
-        LoginPage loginPage = new LoginPage(driver);
-
         loginPage.enterUsername(ParameterProvider.get("login.username"))
                 .enterPassword(ParameterProvider.get("login.password"))
                 .enterUsernameDescription(ParameterProvider.get("login.username.description"));
@@ -38,9 +44,6 @@ public class LoginPageTest extends BaseTest {
 
     @Test
     public void shouldDisplayErrorWhenUsingInvalidCredentials() {
-        driver.get(ParameterProvider.get("login.url"));
-        LoginPage loginPage = new LoginPage(driver);
-
         loginPage.enterUsername(ParameterProvider.get("login.invalid.username"))
                 .enterPassword(ParameterProvider.get("login.invalid.password"))
                 .enterUsernameDescription(ParameterProvider.get("login.username.description"));
@@ -52,5 +55,21 @@ public class LoginPageTest extends BaseTest {
         String expectedMessage = ParameterProvider.get("login.error.message");
         String actualMessage = loginPage.getErrorMessageText();
         Assert.assertEquals(expectedMessage, actualMessage, "Error message should be " + expectedMessage);
+    }
+
+    @Test
+    public void shouldLogoutSuccessfullyAndReturnToLoginPage() {
+        loginPage.enterUsername(ParameterProvider.get("login.username"))
+                .enterPassword(ParameterProvider.get("login.password"))
+                .enterUsernameDescription(ParameterProvider.get("login.username.description"));
+
+        Assert.assertTrue(loginPage.isLoginButtonEnabled(), "Login button should be enabled before logging in");
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isSuccessMessageVisible(), "Should be logged in successfully before logout");
+        loginPage.clickLogout();
+
+        Assert.assertTrue(loginPage.isPageLoaded(), "Login page should be loaded after logout");
+        Assert.assertTrue(loginPage.isUsernameFieldVisible(), "Username field should be visible after logout");
+        Assert.assertTrue(loginPage.isPasswordFieldVisible(), "Password field should be visible after logout");
     }
 }
