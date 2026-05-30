6fe380f1c7dff4db9dd71ea6f8c1fcb3aa998ad3
Nikita Haritonov <nikita.harit@mail.ru>
2026-05-30 19:51:23 +0300

    feat(utils): add TestDataGenerator for random test data

---
+++ src/test/java/com/haritonov/uitests/utils/TestDataGenerator.java
@@ -0,0 +1,36 @@
+package com.haritonov.uitests.utils;
+
+import com.github.javafaker.Faker;
+import com.haritonov.uitests.helpers.ParameterProvider;
+
+public class TestDataGenerator {
+
+    private static final Faker faker = new Faker();
+
+    public static String getRandomFirstName() {
+        return faker.name().firstName();
+    }
+
+    public static String getRandomLastName() {
+        return faker.name().lastName();
+    }
+
+    public static String getRandomEmail() {
+        return faker.internet().emailAddress();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password(
+                Integer.parseInt(ParameterProvider.get("faker.password.min.size")),
+                Integer.parseInt(ParameterProvider.get("faker.password.max.size")));
+    }
+
+    public static String getRandomGender() {
+        return faker.options()
+                .option(
+                        ParameterProvider.get("faker.gender.male"),
+                        ParameterProvider.get("faker.gender.female"),
+                        ParameterProvider.get("faker.gender.other")
+                );
+    }
+}
