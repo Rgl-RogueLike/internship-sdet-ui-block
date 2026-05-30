89100cfb586433301e274df60d991c50d849e878
Nikita Haritonov <nikita.harit@mail.ru>
2026-05-30 10:21:46 +0300

    test(navigation): add menu navigation to Lifetime Membership test

--- src/test/java/com/haritonov/uitests/tests/navigation/NavigationTest.java
+++ src/test/java/com/haritonov/uitests/tests/navigation/NavigationTest.java
@@ -1,6 +1,8 @@
 package com.haritonov.uitests.tests.navigation;
 
+import com.haritonov.uitests.helpers.ParameterProvider;
 import com.haritonov.uitests.pages.HomePage;
+import com.haritonov.uitests.pages.LifetimeMembershipPage;
 import com.haritonov.uitests.tests.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,5 +16,16 @@ public class NavigationTest extends BaseTest {
         Assert.assertTrue(homePage.isNavigationVisible(), "Navigation menu should be visible at the top the page");
         homePage.scrollToFooter();
         Assert.assertTrue(homePage.isNavigationVisible(), "Navigation menu should be visible after scrolling down");
+    }
+
+    @Test
+    public void shouldNavigateToLifetimeMembershipFromMenu() {
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isPageLoaded(), "Home page should be loaded");
+        LifetimeMembershipPage lifetimeMembershipPage = homePage.goToLifetimeMembership();
+        Assert.assertTrue(lifetimeMembershipPage.isPageLoaded(), "Should be on Lifetime Member page");
+        String titleContains = ParameterProvider.get("navigation.lifetime.page.title");
+        String titlePage = lifetimeMembershipPage.getTextTitle().toUpperCase();
+        Assert.assertTrue(titlePage.contains(titleContains), "Page title should contain " + titleContains + ", but got: " + titlePage);
     }
 }
