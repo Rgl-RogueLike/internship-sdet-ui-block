852ce62a8aaf6831fea715eb0403606ac40d8bfe
Nikita Haritonov <nikita.harit@mail.ru>
2026-05-31 10:46:01 +0300

    test(banking): add test for successful customer addition in Bank Manager

---
+++ src/test/java/com/haritonov/uitests/tests/banking/BankingManagerPageTest.java
@@ -0,0 +1,38 @@
+package com.haritonov.uitests.tests.banking;
+
+import com.haritonov.uitests.helpers.ParameterProvider;
+import com.haritonov.uitests.pages.BankManagerPage;
+import com.haritonov.uitests.pages.BankingHomePage;
+import com.haritonov.uitests.tests.BaseTest;
+import com.haritonov.uitests.utils.TestDataGenerator;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BankingManagerPageTest extends BaseTest {
+
+    BankManagerPage managerPage;
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        driver.get(ParameterProvider.get("banking.url"));
+        BankingHomePage bankingHomePage = new BankingHomePage(driver);
+        bankingHomePage.isPageLoaded();
+        managerPage = bankingHomePage.goToBankManagerPage();
+    }
+
+    @Test
+    public void shouldDisplaySuccessAlertWhenAddingCustomerWithValidData() {
+        managerPage.clickAddCustomerButton();
+        managerPage.enterFirstName(TestDataGenerator.getRandomFirstName())
+                .enterLastName(TestDataGenerator.getRandomLastName())
+                .enterPostCode(TestDataGenerator.getRandomPostCode())
+                .submitAddCustomer();
+
+        String expectedMessage = ParameterProvider.get("banking.manager.success.message");
+        String actualMessage = managerPage.acceptAlertAndGetText();
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "Alert should confirm customer addition, but was: " + actualMessage);
+    }
+}
