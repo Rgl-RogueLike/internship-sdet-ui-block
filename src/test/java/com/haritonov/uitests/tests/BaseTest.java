852c3d8948ceda4209ecdb0170b58f180cb366fa
Nikita Haritonov <nikita.harit@mail.ru>
2026-06-01 17:36:19 +0300

    docs(basetest): add javadoc and externalize Chrome arguments to config.properties

--- src/test/java/com/haritonov/uitests/tests/BaseTest.java
+++ src/test/java/com/haritonov/uitests/tests/BaseTest.java
@@ -9,24 +9,34 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+/**
+ * Абстрактный базовый класс для всех тестов.
+ * <p>Перед каждым тестом запускает Chrome с нужными опциями,
+ * создаёт экземпляр {@link Waiter} и переходит на базовый URL.
+ * После теста закрывает браузер.
+ */
 public abstract class BaseTest {
 
     protected WebDriver driver;
     protected Waiter waiter;
 
+    /**
+     * Инициализирует WebDriver, Waiter и открывает главную страницу.
+     */
     @BeforeMethod
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-features=PasswordLeakDetection");
-        options.addArguments("--disable-password-manager-reauthentication");
+        String[] args = ParameterProvider.get("browser.chrome.arguments").split("\\s*,\\s*");
+        options.addArguments(args);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         waiter = new Waiter(driver);
         driver.get(ParameterProvider.get("base.url"));
     }
 
+    /**
+     * Закрывает браузер после каждого теста.
+     */
     @AfterMethod
     public void afterSuite() {
         if (driver != null) {
